cd74f050fdc0f701eea5f5b8d20d6c4b9699289a
Simon Chae <s13chae@gmail.com>
2018-06-25 13:21:10 -0700

    finished

--- server.js
+++ server.js
@@ -5,12 +5,30 @@
 console.log('Hello Noteful!');
 
 const express = require('express');
-
 const data = require('./db/notes');
-
 const app = express();
 
 app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+  const searchTerm = req.query.searchTerm;
+  if (searchTerm) {
+    let answer = data.filter(el => {
+      return el.title.includes(searchTerm);
+    });
+    res.json(answer);
+  } else {
+    res.json(data);
+  }
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  const answer = data.find(el => {
+    return el.id === Number(id);
+  });
+  res.json(answer);
+});
 
 app.listen(8080, function () {
   console.info(`Server listening on ${this.address().port}`);
