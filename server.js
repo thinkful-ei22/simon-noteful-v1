47ee7bffcdc498d8e570414145d8394d4534ee8f
Simon Chae <s13chae@gmail.com>
2018-06-26 14:18:33 -0700

    completed last exercise

--- server.js
+++ server.js
@@ -4,17 +4,22 @@
 
 console.log('Hello Noteful!');
 
-const logConsole = require('./middleware/logger');
-const {PORT} = require('./config');
-
 const express = require('express');
+
 const data = require('./db/notes');
 const simDB = require('./db/simDB');  
 const notes = simDB.initialize(data); 
+
+const logConsole = require('./middleware/logger');
+const {PORT} = require('./config');
+
 const app = express();
 
 app.use(logConsole);
+
 app.use(express.static('public'));
+
+app.use(express.json());
 
 app.get('/api/notes', (req, res, next) => {
   const searchTerm = req.query.searchTerm;
@@ -32,14 +37,48 @@ app.get('/api/notes/:id', (req, res, next) => {
     if (err) {
       return next(err);
     }
-    res.json(item);
+    if (item) {
+      res.json(item);
+    } else {
+      next();
+    }
   });
 });
 
+app.put('/api/notes/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  /***** Never trust users - validate input *****/
+  const updateObj = {};
+  const updateFields = ['title', 'content'];
+
+  updateFields.forEach(field => {
+    if (field in req.body) {
+      updateObj[field] = req.body[field];
+    }
+  });
+
+  notes.update(id, updateObj, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.json(item);
+    } else {
+      next();
+    }
+  });
+});
+
+app.get('/boom', (req, res, next) => {
+  throw new Error('Boom!!');
+});
+
+
 app.use(function (req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
-  res.status(404).json({ message: 'Not Found' });
+  res.status(404).json({ message: 'Not Found'});
 });
 
 app.use(function (err, req, res, next) {
