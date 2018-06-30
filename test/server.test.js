957f94071d2f04e642cb803cc96443b9479a9f04
Simon Chae <s13chae@gmail.com>
2018-06-29 22:25:13 -0700

    added more tests

--- test/server.test.js
+++ test/server.test.js
@@ -7,20 +7,18 @@ const expect = chai.expect;
 
 chai.use(chaiHttp);
 
-describe('Reality check', function () {
 
+describe('Reality check', function () {
   it('true should be true', function () {
     expect(true).to.be.true;
   });
-
   it('2 + 2 should equal 4', function () {
     expect(2 + 2).to.equal(4);
   });
-
 });
 
-describe('Express static', function () {
 
+describe('Express static', function () {
   it('GET request "/" should return the index page', function () {
     return chai.request(app)
       .get('/')
@@ -33,8 +31,8 @@ describe('Express static', function () {
 
 });
 
-describe('404 handler', function () {
 
+describe('404 handler', function () {
   it('should respond with 404 when given a bad path', function () {
     return chai.request(app)
       .get('/DOES/NOT/EXIST')
@@ -45,6 +43,7 @@ describe('404 handler', function () {
   });
 
 });
+
 
 describe('GET /api/notes', function() {
   it ('should return the default of 10 Notes as an array', function() {
@@ -94,8 +93,8 @@ describe('GET /api/notes', function() {
   });
 });
 
-describe('GET /api/notes/:id', function () {
 
+describe('GET /api/notes/:id', function () {
   it('should return correct notes', function () {
     return chai.request(app)
       .get('/api/notes/1000')
@@ -107,7 +106,6 @@ describe('GET /api/notes/:id', function () {
         expect(res.body.id).to.equal(1000);
       });
   });
-
   it('should respond with a 404 for an invalid id', function () {
     return chai.request(app)
       .get('/api/notes/DOESNOTEXIST')
@@ -117,4 +115,104 @@ describe('GET /api/notes/:id', function () {
       });
   });
 
+
+  describe('POST /api/notes', function () {
+    it('should create and return a new item when provided valid data', function () {
+      const newItem = {
+        'title': 'The best article about cats ever!',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...'
+      };
+      return chai.request(app)
+        .post('/api/notes')
+        .send(newItem)
+        .then(function (res) {
+          expect(res).to.have.status(201);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a('object');
+          expect(res.body).to.include.keys('id', 'title', 'content');
+          expect(res.body.id).to.equal(1010);
+          expect(res.body.title).to.equal(newItem.title);
+          expect(res.body.content).to.equal(newItem.content);
+          expect(res).to.have.header('location');
+        });
+    });
+    it('should return an error when missing "title" field', function () {
+      const newItem = {
+        'foo': 'bar'
+      };
+      return chai.request(app)
+        .post('/api/notes')
+        .send(newItem)
+        .catch(err => err.response)
+        .then(res => {
+          expect(res).to.have.status(400);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a('object');
+          expect(res.body.message).to.equal('Missing `title` in request body');
+        });
+    });
+  });
+
+
+  describe('PUT /api/notes/:id', function () {
+    it('should update the note', function () {
+      const updateItem = {
+        'title': 'What about dogs?!',
+        'content': 'woof woof'
+      };
+      return chai.request(app)
+        .put('/api/notes/1005')
+        .send(updateItem)
+        .then(function (res) {
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a('object');
+          expect(res.body).to.include.keys('id', 'title', 'content');
+
+          expect(res.body.id).to.equal(1005);
+          expect(res.body.title).to.equal(updateItem.title);
+          expect(res.body.content).to.equal(updateItem.content);
+        });
+    });
+    it('should respond with a 404 for an invalid id', function () {
+      const updateItem = {
+        'title': 'What about dogs?!',
+        'content': 'woof woof'
+      };
+      return chai.request(app)
+        .put('/api/notes/DOESNOTEXIST')
+        .send(updateItem)
+        .catch(err => err.response)
+        .then(res => {
+          expect(res).to.have.status(404);
+        });
+    });
+    it('should return an error when missing "title" field', function () {
+      const updateItem = {
+        'foo': 'bar'
+      };
+      return chai.request(app)
+        .put('/api/notes/1005')
+        .send(updateItem)
+        .catch(err => err.response)
+        .then(res => {
+          expect(res).to.have.status(400);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a('object');
+          expect(res.body.message).to.equal('Missing `title` in request body');
+        });
+    });
+
+  });
+
+
+  describe('DELETE  /api/notes/:id', function () {
+    it('should delete an item by id', function () {
+      return chai.request(app)
+        .delete('/api/notes/1005')
+        .then(function (res) {
+          expect(res).to.have.status(204);
+        });
+    });
+  });
 });
